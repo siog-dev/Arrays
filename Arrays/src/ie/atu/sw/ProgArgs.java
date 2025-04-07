50ef6d679ebfc9f0295bf74b870e3f5a3d82660f
siog <siog@LAPTOP-545TJ6PE>
2025-04-07 21:01:35 +0100

    Add comment describing steps for running arguments in the array

--- Arrays/src/ie/atu/sw/ProgArgs.java
+++ Arrays/src/ie/atu/sw/ProgArgs.java
@@ -5,7 +5,22 @@ public class ProgArgs {
 	public static void main(String[] args) {
 		System.out.println(args + "---->" + args.length);
 		//Prints: "[Ljava.lang.String;@4517d9a3---->0"
-
+		
+		/*
+		 * In Run>Run Configurations>Arguments Tab>In Program Arguments: 
+		 * type "The Quick Brown Fox Jumps Over The Lazy Dog" and hit Enter
+		 * 
+		 * Console Prints: [Ljava.lang.String;@4517d9a3---->9
+		 * 
+		 * 
+		 * Do the same in a terminal
+		 * cd Arrays/bin/
+		 * java ie.atu.sw.ProgArgs The Quick Brown Fox
+		 * 
+		 * Terminal Prints: [Ljava.lang.String;@372f7a8d---->4
+		 */
+		
+		
 	}
 	
 }

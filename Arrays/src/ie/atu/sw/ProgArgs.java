ffb7d7aad23e99d13fdc89583b3a0b1d4074f75c
siog <siog@LAPTOP-545TJ6PE>
2025-04-07 20:51:00 +0100

    Comment the print message
    Prints: "[Ljava.lang.String;@4517d9a3---->0"

--- Arrays/src/ie/atu/sw/ProgArgs.java
+++ Arrays/src/ie/atu/sw/ProgArgs.java
@@ -4,6 +4,8 @@ public class ProgArgs {
 	
 	public static void main(String[] args) {
 		System.out.println(args + "---->" + args.length);
+		//Prints: "[Ljava.lang.String;@4517d9a3---->0"
+
 	}
 	
 }

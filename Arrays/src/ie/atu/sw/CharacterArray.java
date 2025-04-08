fe47ce89df90ec7c870a3deb11eee831bcb4db07
siog <siog@LAPTOP-545TJ6PE>
2025-04-08 20:56:18 +0100

    Create a contructor method and instance variable

--- Arrays/src/ie/atu/sw/CharacterArray.java
+++ Arrays/src/ie/atu/sw/CharacterArray.java
@@ -1,7 +1,14 @@
 package ie.atu.sw;
 
 public class CharacterArray {
-
+	
+	//Instance Variables
+	private char[] text = null;
+	
+	//Create a contructor that will take in user data and create an array
+	public CharacterArray(int size) {
+		
+	}
 	
 	public static void main(String[] args) {
 		
